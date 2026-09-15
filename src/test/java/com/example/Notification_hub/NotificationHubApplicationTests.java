05e0bd3ba7bd1ca6432d64aba15fd2615da8c4c8
atgqlo <mihailovart3mka@yandex.ru>
2026-09-16 02:43:16 +0500

    init

---
+++ src/test/java/com/example/Notification_hub/NotificationHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Notification_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
