ba68a8fbd7949358520801bedf1d925af9187f4d
atgqlo <mihailovart3mka@yandex.ru>
2026-09-16 23:59:32 +0500

    add repository

---
+++ src/main/java/com/example/Notification_hub/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.example.Notification_hub.repository;
+
+
+import com.example.Notification_hub.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
