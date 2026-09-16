b0771632f14beeebd8db4466b441c4bf13740fc9
atgqlo <mihailovart3mka@yandex.ru>
2026-09-17 02:27:04 +0500

    add getByID

--- src/main/java/com/example/Notification_hub/controller/NotificationController.java
+++ src/main/java/com/example/Notification_hub/controller/NotificationController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/notifications")
@@ -25,4 +22,14 @@ public class NotificationController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedNotification);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Notification> getNotificationById(@PathVariable Long id){
+        Notification notification = service.getNotificationByID(id);
+        return ResponseEntity.ok(notification);
+    }
+
+
+
+
 }
